ae774d722772a7e48769809ec651ebd59e0b47df
Jose Martinez <Josepepemtz1.jm@gmail.com>
2021-09-22 15:56:19 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CurrencyInput from './CurrencyInput'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CurrencyConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rate: 0.89,
+      usd: 1,
+      euro: 1 * 0.89,
+    };
+
+    this.handleUsdChange = this.handleUsdChange.bind(this);
+    this.handleEuroChange = this.handleEuroChange.bind(this);
+  }
+
+  toUsd(amount, rate) { // inputs eur and returns usd
+    return amount * (1 / rate);
+  }
+
+  toEuro(amount, rate) { // inputs usd and returns eur
+    return amount * rate;
+  }
+
+  handleUsdChange(event) {
+    const euro = this.convert(event.target.value, this.state.rate, this.toEuro);
+
+    this.setState({
+      usd: event.target.value,
+      euro
+    });
+  }
+
+  handleEuroChange(event) {
+    const usd = this.convert(event.target.value, this.state.rate, this.toUsd);
+
+    this.setState({
+      euro: event.target.value,
+      usd
+    });
+  }
+
+  convert(amount, rate, equation) {
+    const input = parseFloat(amount);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    return equation(input, rate).toFixed(3);
+  }
+
+  render() {
+    const { rate, usd, euro } = this.state;
+
+    return (
+      <div className="container">
+        <div className="text-center p-3 mb-2">
+          <h2 className="mb-2">Currency Converter</h2>
+          <h4>USD 1 : {rate} EURO</h4>
+        </div>
+        <div className="row text-center">
+          <div className="col-12">
+            <span className="mr-1">USD</span>
+            <CurrencyInput value={usd} handleChange={this.handleUsdChange} />
+            <span className="mx-3">=</span>
+            <CurrencyInput value={euro} handleChange={this.handleEuroChange} />
+            <span className="ml-1">EURO</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default CurrencyConverter;
